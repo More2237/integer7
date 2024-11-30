30cc554f37e83a0345c09713d982934b9b8ec80f
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-30 20:23:46 +0500

    kjhgc

---
+++ integer7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace integer7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a = int.Parse(Console.ReadLine());
+           int b = a/10;
+           int c = a%10;
+
+
+           var result = b + c;
+           var result1 = c * b;
+           Console.WriteLine(result);
+           Console.WriteLine(result1);
+
+        }
+    }
+}
